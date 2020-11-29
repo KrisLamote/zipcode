d1702c3ca0f1c030971c0d8cf97f5a9e80c09691
Kris Lamote <kris.lamote@it-kitchen.be>
2020-11-29 10:12:41 +0100

    app: simple country rules file + spot check tests

--- parse/yaml.go
+++ parse/yaml.go
@@ -5,9 +5,9 @@ import (
 )
 
 // Parse parses a yaml file into a set of rules
-func Parse(f string) (map[string][]string, error) {
+func Parse(data []byte) (map[string][]string, error) {
 	var rules map[string][]string
-	err := yaml.Unmarshal([]byte(f), &rules)
+	err := yaml.Unmarshal(data, &rules)
 	if err != nil || len(rules) == 0 {
 		return map[string][]string{}, err
 	}
